8401f10655079959607eb28789fd5a6ffb81ee64
Joebomb34 <jpbombassei@email.neit.edu>
2022-05-10 10:47:29 -0400

    SE266

--- week7/model/model_disney.php
+++ week7/model/model_disney.php
@@ -1,47 +1,74 @@
 <?php
-// Example functions for the Disney Character Voting app 
+// Example class for the Disney Character Voting app 
 
-    include (__DIR__ . '/db.php');
-    
+class DisneyVotes
+{
+   // This data field represents the database
+   private $voteData;
+
+   //*****************************************************
+   // Teams class constructor:
+   // Instantiates a PDO object based on given URL and
+   // uses that to initialize the data field $teamData
+   //
+   // INPUT: URL of database configuration file
+   // Throws exception if database access fails
+   // ** This constructor is very generic and can be used to 
+   // ** access your course database for any assignment
+   // ** The methods need to be changed to hit the correct table(s)
+   public function __construct($configFile) 
+   {
+       // Parse config file, throw exception if it fails
+       if ($ini = parse_ini_file($configFile))
+       {
+           // Create PHP Database Object
+           $votePDO = new PDO( "mysql:host=" . $ini['servername'] . 
+                               ";port=" . $ini['port'] . 
+                               ";dbname=" . $ini['dbname'], 
+                               $ini['username'], 
+                               $ini['password']);
+
+           // Don't emulate (pre-compile) prepare statements
+           $votePDO->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+           // Throw exceptions if there is a database error
+           $votePDO->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+           //Set our database to be the newly created PDO
+           $this->voteData = $votePDO;
+       }
+       else
+       {
+           // Things didn't go well, throw exception!
+           throw new Exception( "<h2>Creation of database object failed!</h2>", 0, null );
+       }
+
+   } // end constructor
+
     // Retrieves all Disney characters in the database
-    function getCharacters ()
+    function getCharacters()
     {
-         global $db;
-         $stmt = $db->prepare("SELECT DisneyCharacterId, DisneyCharacterName, DisneyCharacterImage FROM DisneyCharacters ORDER BY DisneyCharacterName");
+        $voteTable = $this->voteData;   // Alias for database PDO
+        $stmt = $voteTable->prepare("SELECT DisneyCharacterId, DisneyCharacterName, DisneyCharacterImage FROM DisneyCharacters ORDER BY DisneyCharacterName");
 
         $characters = array();
-        if ($stmt->execute() && $stmt->rowCount() > 0) {
+        if ($stmt->execute() && $stmt->rowCount() > 0) 
+        {
             $characters = $stmt->fetchAll(PDO::FETCH_ASSOC);
         }
-        return ($characters);
+        return $characters;
     }
-    
-   
-    // Adds a vote for the charater whose ID is sent
-    function insertDisneyVote ($character_id) {
-        global $db;
-        $stmt = $db->prepare("INSERT INTO DisneyVotes SET DisneyCharacterID = :character_id");
-        $results = "Not added";
-        $binds = array(
-            ":character_id" => $character_id
-        );
 
-
-        if ($stmt->execute($binds) && $stmt->rowCount() > 0) {
-            $results = 'Data Added';
-        }
-        
-        return (getVotes());
-    }
-    
     // Gets total of votes for each character
     function getVotes() 
     {
-        global $db;
+        $results = [];
+        $voteTable = $this->voteData;   // Alias for database PDO
+
          $sql = "SELECT DisneyCharacterName, COUNT(*) AS VoteCount FROM DisneyCharacters c 
                 LEFT OUTER JOIN DisneyVotes v ON c.DisneyCharacterID=v.DisneyCharacterID
                 GROUP BY DisneyCharacterName ORDER BY v.DisneyCharacterID ";
-        $stmt = $db->query ($sql);
+        $stmt = $voteTable->query ($sql);
        
        $votes = $stmt->fetchAll(PDO::FETCH_ASSOC);
        $results[0] = array(); // list characters
@@ -54,11 +81,28 @@
        
         return (json_encode($results));
     }
-
-    // run this code to test instering a vote
-//    $test = insertDisneyVote (2);
-//    echo $test;
-   
-  
     
+   
+    // Adds a vote for the charater whose ID is sent
+    function insertDisneyVote($character_id) 
+    {
+        $voteTable = $this->voteData;   // Alias for database PDO
+
+        $stmt = $voteTable->prepare("INSERT INTO DisneyVotes SET DisneyCharacterID = :character_id");
+        $results = "Not added";
+        $binds = array(
+            ":character_id" => $character_id
+        );
+
+        if ($stmt->execute($binds) && $stmt->rowCount() > 0) 
+        {
+            $results = 'Data Added';
+        }
+        
+        return ($this->getVotes());
+    }
+    
+    
+ 
+}
     
